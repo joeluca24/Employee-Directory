2841f3af40d1fc65268d2fa315aff25008173d79
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-10-22 19:32:44 -0400

    created table for app

--- src/App.js
+++ src/App.js
@@ -13,11 +13,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <p>{randomName}</p>
-         <p>{JSON.stringify(Fakermod)}</p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table>
+  <tr>
+    <th>email</th>
+    <th>phone number</th>
+    <th>name</th>
+    <th>image</th>
+  </tr>
+  {Fakermod.map(employee => 
+    <tr>
+      <td>{employee.email}</td>
+  <td>{employee.phoneNumber}</td>
+  <td>{employee.name}</td>
+      <td><img src={employee.image}/></td>
+    </tr>
+    )}
+  </table>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
