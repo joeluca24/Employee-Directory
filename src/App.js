fc508690c5ee1ee31eb5b60ef4a71cc36e312043
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-10-28 21:24:23 -0400

    fixed styling for page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Faker from "faker";
 import Fakermod from "./utils/fakemod";
@@ -11,34 +10,27 @@ function App() {
   var randomName = Faker.name.findName(); // Rowan Nikolau
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <table>
-  <tr>
-    <th>email</th>
-    <th>phone number</th>
-    <th>name</th>
-    <th>image</th>
-  </tr>
-  {Fakermod.map(employee => 
-    <tr>
-      <td>{employee.email}</td>
-  <td>{employee.phoneNumber}</td>
-  <td>{employee.name}</td>
-      <td><img src={employee.image}/></td>
-    </tr>
-    )}
-  </table>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <table>
+        <tr>
+          <th>email</th>
+          <th>phone number</th>
+          <th>name</th>
+          <th>image</th>
+        </tr>
+        {Fakermod.map(employee =>
+          <tr>
+            <td>{employee.email}</td>
+            <td>{employee.phoneNumber}</td>
+            <td>{employee.name}</td>
+            <td><img src={employee.image} /></td>
+          </tr>
+        )}
+      </table>
+
+
+
     </div>
   );
 }
